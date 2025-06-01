239706277aed3a2f11f48776b307ddd772a971b8
Dawit <tesfayedawit22090582@gmail.com>
2025-06-01 15:32:42 +0300

    auth functionality

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,18 +1,59 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./Auth.module.css";
 import { Link } from "react-router-dom";
 import iconBlack from "./icon/iconBlack.png";
 import { auth } from "../../Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { DataContext } from "../../components/DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
 
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  console.log(email, password);
+  const [{ user }, dispatch] = useContext(DataContext);
+  console.log(user);
+
+  const authHandler = (e) => {
+    e.preventDefault();
+    const action = e.target.name;
+
+    if (!email || !password) {
+      alert("Please enter both email and password");
+      return;
+    }
+
+    if (action === "signin") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          dispatch({
+            type: Type.SET_USER,
+            user: userCredential.user,
+          });
+        })
+        .catch((error) => {
+          console.log("Sign-in error:", error.message);
+        });
+    } else if (action === "signup") {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          dispatch({
+            type: Type.SET_USER,
+            user: userCredential.user,
+          });
+        })
+        .catch((error) => {
+          console.log("Sign-up error:", error.message);
+        });
+    }
+  };
 
   return (
     <section className={classes.auth}>
-      {/* logo */}
       <Link to="/">
         <img src={iconBlack} alt="App Logo" />
       </Link>
@@ -24,6 +65,7 @@ function Auth() {
           <div>
             <label htmlFor="email">Email</label>
             <input
+              placeholder="Enter your email"
               type="email"
               id="email"
               value={email}
@@ -34,6 +76,7 @@ function Auth() {
           <div>
             <label htmlFor="password">Password</label>
             <input
+              placeholder="Enter your password"
               type="password"
               id="password"
               value={password}
@@ -41,18 +84,28 @@ function Auth() {
             />
           </div>
 
-          <button type="submit" className={classes.signin_btn}>
+          <button
+            onClick={authHandler}
+            type="submit"
+            name="signin"
+            className={classes.signin_btn}
+          >
             Sign In
           </button>
         </form>
 
         <p>
-          By signing up, you agree to the Amazon clone's
-          <Link to="/terms">Terms of Use</Link> and
+          By signing up, you agree to the Amazon clone's{" "}
+          <Link to="/terms">Terms of Use</Link> and{" "}
           <Link to="/privacy">Privacy Policy</Link>.
         </p>
 
-        <button type="submit" className={classes.signup_btn}>
+        <button
+          onClick={authHandler}
+          type="button"
+          name="signup"
+          className={classes.signup_btn}
+        >
           Sign Up
         </button>
       </div>
