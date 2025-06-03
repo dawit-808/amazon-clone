414b1f10d8e2989e0605bd3af519cf6dc3eee57a
Dawit <tesfayedawit22090582@gmail.com>
2025-06-03 15:14:06 +0300

    backend payment api

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "success!",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = Number(req.query.total);
+
+  if (total > 0) {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+      });
+
+      res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(403).json({
+      message: "Total must be greater than 0",
+    });
+  }
+});
+
+app.listen(5000, (err) => {
+  if (err) throw err;
+  console.log("Server running on http://localhost:5000");
+});
