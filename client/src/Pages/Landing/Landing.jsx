aedc221d8608cbb4a93331d8cba58364aadeaa04
Dawit <tesfayedawit22090582@gmail.com>
2025-06-08 10:19:59 +0300

    bgc change

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -3,14 +3,17 @@ import CarouselEffect from "../../components/CarouselEffect/CarouselEffect";
 import Category from "../../components/Category/Category";
 import LayOut from "../../components/LayOut/LayOut";
 import Product from "../../components/Product/Product";
+import classes from "./Landing.module.css";
 
 function Landing() {
   return (
-    <LayOut>
-      <CarouselEffect />
-      <Category />
-      <Product />
-    </LayOut>
+    <section className={classes.landing_section}>
+      <LayOut>
+        <CarouselEffect />
+        <Category />
+        <Product />
+      </LayOut>
+    </section>
   );
 }
 
